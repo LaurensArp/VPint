33b16a9887ab648f6b3c2e325e1372a818671fb7
Laurens Arp <l.r.arp@liacs.leidenuniv.nl>
2021-02-20 12:29:04 +0100

    Better inheritance

---
+++ MRPinterpolation/MRP.py
@@ -0,0 +1,370 @@
+"""Module for MRP-based interpolation.
+"""
+
+import numpy as np
+import networkx as nx
+import datetime
+
+class MRP:
+    """
+    Basic MRP class containing common functionality of SMRP and STMRP.
+
+    Attributes
+    ----------
+    original_grid : 2D numpy array
+        the original grid supplied to be interpolated
+    pred_grid : 2D numpy array
+        interpolated version of original_grid
+    _dims : the number of dimensions for this MRP (2 for spatial, 3 for temporal)
+
+    Methods
+    -------
+    reset():
+        Returns pred_grid and G to their original state
+        
+    get_pred_grid():
+        Returns pred_grid
+        
+    mean_absolute_error():
+        Computes the mean absolute error of pred_grid compared to a given ground truth grid
+    """
+    
+    def __init__(self,grid):
+        self.original_grid = grid.copy()
+        self.dims = len(grid.shape)
+        self.init_pred_grid()
+        
+        
+    def __str__(self):
+        return(str(self.pred_grid))
+    
+    
+    def reset(self):
+        """Return prediction grid and graph representation to their original form"""
+        self.pred_grid = self.original_grid
+        self.G = self.to_graph()
+          
+    
+    def init_pred_grid(self):
+        """Initialise pred_grid with mean values as initial values for missing cells"""
+        self.pred_grid = self.original_grid.copy()
+        height = self.pred_grid.shape[0]
+        width = self.pred_grid.shape[1]
+        if(self.dims == 3):
+            depth = self.pred_grid.shape[2]
+        mean = np.nanmean(self.original_grid)
+        for i in range(0,height):
+            for j in range(0,width):
+                if(self.dims == 3):
+                    for t in range(0,depth):
+                        if(np.isnan(self.pred_grid[i,j,t])):
+                            self.pred_grid[i,j,t] = mean
+                else:
+                    if(np.isnan(self.pred_grid[i,j])):
+                        self.pred_grid[i,j] = mean
+             
+               
+    def get_pred_grid(self):
+        """Return pred_grid"""
+        self.update_grid()
+        return(self.pred_grid)
+        
+        
+    def mean_absolute_error(self,true_grid,gridded=False):
+        """
+        Compute the mean absolute error of pred_grid given true_grid as ground truth
+        
+        :param true_grid: ground truth for all grid cells
+        :param gridded: optional Boolean specifying whether to return an error grid with the MAE
+        :returns: mean absolute error, optionally a tuple of MAE and per-cell error
+        """
+        height = self.pred_grid.shape[0]
+        width = self.pred_grid.shape[1]
+        if(self.dims == 3):
+            depth = self.pred_grid.shape[2]
+            error_grid = np.zeros((height,width,depth))
+        else:
+            error_grid = np.zeros((height,width))
+        
+        e = 0
+        c = 0
+        for i in range(0,height):
+            for j in range(0,width):
+                if(self.dims == 3):
+                    for t in range(0,depth):
+                        if(np.isnan(self.original_grid[i][j][t])):
+                            err = abs(self.pred_grid[i][j][t] - true_grid[i][j][t])
+                            error_grid[i][j][t] = err
+                            e += err
+                            c += 1
+                        else:
+                            error_grid[i][j][t] = np.nan
+                else:
+                    if(np.isnan(self.original_grid[i][j])):
+                        err = abs(self.pred_grid[i][j] - true_grid[i][j])
+                        error_grid[i][j] = err
+                        e += err
+                        c += 1
+                    else:
+                        error_grid[i][j] = np.nan
+        
+        mae = e/c
+        if(gridded):
+            result = (mae,error_grid)
+        else:
+            result = mae
+            
+        return(result)
+    
+
+class SMRP(MRP):
+    """
+    Basic class implementing basic functions used by SD-MRP and WP-MRP.
+
+    Attributes
+    ----------
+    original_grid : 2D numpy array
+        the original grid supplied to be interpolated
+    pred_grid : 2D numpy array
+        interpolated version of original_grid
+    G : networkx directed graph
+        graph representation of pred_grid
+
+    Methods
+    -------
+    reset():
+        Returns pred_grid and G to their original state
+        
+    to_graph():
+        Converts original_grid to a graph representation
+        
+    update_grid():
+        Updates pred_grid to reflect changes to G
+        
+    get_pred_grid():
+        Returns pred_grid
+    """
+    
+    def __init__(self,grid):
+        super().__init__(grid)
+        self.G = self.to_graph()
+        
+        
+    def to_graph(self):
+        """
+        Converts a grid to graph form.
+        
+        :param default_E: default expected value used for initialisation of the MRP
+        """
+        G = nx.DiGraph()
+        
+        grid_height = len(self.original_grid)
+        grid_width = len(self.original_grid[0])
+        
+        for i in range(0,grid_height):
+            for j in range(0,grid_width):
+                val = self.original_grid[i][j]
+                node_name = "r" + str(i) + "c" + str(j)
+                if(np.isnan(val)):
+                    E = np.nanmean(self.original_grid)
+                else:
+                    E = val
+                G.add_node(node_name,y=val,E=E,r=i,c=j)
+
+                # Connect to node above
+                if(i > 0):
+                    neighbour_name = "r" + str(i-1) + "c" + str(j)
+                    G.add_edge(node_name,neighbour_name)
+                    G.add_edge(neighbour_name,node_name)
+                # Connect to node to the left
+                if(j > 0):
+                    neighbour_name = "r" + str(i) + "c" + str(j-1)
+                    G.add_edge(node_name,neighbour_name)
+                    G.add_edge(neighbour_name,node_name)
+
+        return(G)
+    
+    
+    def update_grid(self):
+        """Update pred_grid to reflect changes to G"""
+        for n in self.G.nodes(data=True):
+            r = n[1]['r']
+            c = n[1]['c']
+            E = n[1]['E']
+            self.pred_grid[r][c] = E
+            
+            
+            
+class STMRP(MRP):
+    """
+    Basic class implementing the basic spatio-temporal framework for 
+    SD-MRP and WP-MRP.
+
+    Attributes
+    ----------
+    original_grid : 2D numpy array
+        the original grid supplied to be interpolated
+    pred_grid : 2D numpy array
+        interpolated version of original_grid
+    G : networkx directed graph
+        graph representation of pred_grid
+
+    Methods
+    -------
+    reset():
+        Returns pred_grid and G to their original state
+        
+    dim_check():
+        Checks the dimensions of supplied grid, transforms to
+        3D grid if necessary
+        
+    set_timesteps():
+        Automatically creates a 3D grid from a time-stamped
+        dictionary of 2D spatial grids
+        
+    to_graph():
+        Converts original_grid to a graph representation
+        
+    update_grid():
+        Updates pred_grid to reflect changes to G
+        
+    get_pred_grid():
+        Returns pred_grid
+    """
+
+    def __init__(self,data,auto_timesteps):       
+        if(auto_timesteps):
+            new_grid = self.set_timesteps(data.copy())
+        else:
+            new_grid = self.dim_check(data.copy())
+            
+        super().__init__(new_grid)
+        self.G = self.to_graph()
+           
+    def dim_check(self,grid):
+        """
+        Checks the dimensions of the supplied grid, and transforms
+        it into a 3D grid (at a single time step) if necessary (this
+        retains non-temporal spatial MRP functionality).
+        
+        :param grid: grid to check dimensions of
+        :returns: suitable 3D grid
+        """
+        dims = grid.shape
+        if(grid.ndim == 1):
+            new_grid = np.zeros((dims[0],1,1))
+            new_grid[:,0,0] = grid
+        elif(grid.ndim == 2):
+            new_grid = np.zeros((dims[0],dims[1],1))
+            new_grid[:,:,0] = grid
+        elif(grid.ndim == 3):
+            new_grid = grid
+        return(new_grid)
+    
+    
+    def set_timesteps(self,data):
+        """
+        Checks the dimensions of the supplied grid, and transforms
+        it into a 3D grid (at a single time step) if necessary (this
+        retains non-temporal spatial MRP functionality).
+        
+        :param data: dictionary containing time stamps as keys and
+        2D spatial grids as values. Time stamps MUST be chronologically
+        ordered, and in string format "YYYY-MM-DD HH:MM:SS"
+        :returns: spatio-temporal 3D grid
+        """
+        
+        stamps = []
+        min_stamp = None
+        max_stamp = None
+        min_gap = None
+        i = 0
+        for k,v in data.items():
+            time_obj = datetime.datetime.strptime(k,"%Y-%m-%d %H:%M:%S")
+            stamps.append(time_obj)
+            if(i > 0):
+                diff = time_obj - stamps[i-1]
+                if(min_gap != None):
+                    if(diff < min_gap):
+                        min_gap = diff
+                else:
+                    min_gap = diff
+                max_stamp = time_obj
+                
+            else:
+                min_stamp = time_obj
+                max_stamp = time_obj
+            
+            i += 1
+        
+        # Create empty 3D grid with (max-min)/gap time steps incremented by gap
+        
+        str_ind = min_stamp.strftime("%Y-%m-%d %H:%M:%S")
+        height = data[str_ind].shape[0]
+        width = data[str_ind].shape[1]
+        depth = int((max_stamp - min_stamp) / min_gap) + 1
+        
+        grid = np.empty((height,width,depth))
+        grid[:] = np.nan
+        
+        # Assign 2D grids to appropriate time steps
+        
+        for k,v in data.items():
+            stamp = datetime.datetime.strptime(k,"%Y-%m-%d %H:%M:%S")
+            ind = int((stamp - min_stamp) / min_gap)
+            grid[:,:,ind] = v
+        
+        # Return 3D grid
+        
+        return(grid)
+        
+    def to_graph(self):
+        """
+        Converts a grid to graph form.
+        
+        :param default_E: default expected value used for initialisation of the MRP
+        """
+        G = nx.DiGraph()
+        
+        grid_height = self.original_grid.shape[0]
+        grid_width = self.original_grid.shape[1]
+        grid_depth = self.original_grid.shape[2]
+        
+        for i in range(0,grid_height):
+            for j in range(0,grid_width):
+                for t in range(0,grid_depth):
+                    val = self.original_grid[i][j][t]
+                    if(np.isnan(val)):
+                        E = np.nanmean(self.original_grid)
+                    else:
+                        E = val
+                    node_name = "r" + str(i) + "c" + str(j) + "t" + str(t)
+                    G.add_node(node_name,y=val,E=E,r=i,c=j,t=t)
+
+                    # Connect to node above
+                    if(i > 0):
+                        neighbour_name = "r" + str(i-1) + "c" + str(j) + "t" + str(t)
+                        G.add_edge(node_name,neighbour_name)
+                        G.add_edge(neighbour_name,node_name)
+                    # Connect to node to the left
+                    if(j > 0):
+                        neighbour_name = "r" + str(i) + "c" + str(j-1) + "t" + str(t)
+                        G.add_edge(node_name,neighbour_name)
+                        G.add_edge(neighbour_name,node_name)
+                    # Connect to previous time step
+                    # TODO: user-supplied neighbourhood
+                    if(t > 0):
+                        neighbour_name = "r" + str(i) + "c" + str(j) + "t" + str(t-1)
+                        G.add_edge(node_name,neighbour_name)
+                        G.add_edge(neighbour_name,node_name)
+
+        return(G)
+        
+    def update_grid(self):
+        """Update pred_grid to reflect changes to G"""
+        for n in self.G.nodes(data=True):
+            r = n[1]['r']
+            c = n[1]['c']
+            t = n[1]['t']
+            E = n[1]['E']
+            self.pred_grid[r][c][t] = E
---
+++ MRPinterpolation/SD_MRP.py
@@ -0,0 +1,365 @@
+"""Module for MRP-based spatial interpolation.
+"""
+
+import numpy as np
+import networkx as nx
+
+from .MRP import SMRP, STMRP
+        
+        
+class SD_SMRP(SMRP):
+    """
+    Class for SD-SMRP, extending SMRP
+
+    Attributes
+    ----------
+    original_grid : 2D numpy array
+        the original grid supplied to be interpolated
+    pred_grid : 2D numpy array
+        interpolated version of original_grid
+    G : networkx directed graph
+        graph representation of pred_grid
+    gamma : float
+        discount parameter gamma used by SD-MRP (typically 0-1)
+
+    Methods
+    -------
+    run():
+        Runs SD-MRP
+        
+    set_gamma():
+        Sets gamma to a user-specified value
+        
+    find_gamma():
+        Automatically determines the best gamma (using subsampling or a training set)
+        
+    compute_confidence():
+        compute an indication of uncertainty per pixel in pred_grid
+    """
+    
+    def __init__(self,grid,gamma=0.9):
+        super().__init__(grid)
+        self.gamma = gamma
+    
+    
+    def set_gamma(self,gamma):
+        """
+        Sets gamma to the manually supplied value.
+        
+        :param gamma: user-supplied gamma value
+        """
+        self.gamma = gamma
+    
+            
+    def run(self,iterations):
+        """
+        Runs SD-SMRP for the specified number of iterations.
+        
+        :param iterations: number of iterations used for the state value update function
+        :returns: interpolated grid pred_grid
+        """
+
+        for n in self.G.nodes(data=True):
+            r = n[1]['r']
+            c = n[1]['c']
+            y = n[1]['y']
+            E = n[1]['E']
+            
+            if(np.isnan(y)):
+                v_a_sum = 0
+                for n1,n2,w in self.G.in_edges(n[0],data=True):
+                    destination_node = self.G.nodes(data=True)[n1]
+                    E_dest = destination_node['E']
+                    v_a = self.gamma * E_dest
+                    v_a_sum += v_a
+                E_new = v_a_sum / len(self.G.in_edges(n[0]))
+                nx.set_node_attributes(self.G,{n[0]:E_new},'E')
+
+            else:
+                nx.set_node_attributes(self.G,{n[0]:y},'E')
+        
+        self.update_grid()
+        return(self.pred_grid)
+    
+    
+    def run_experimental(self,iterations):
+        height = self.pred_grid.shape[0]
+        width = self.pred_grid.shape[1]
+        
+        for it in range(0,iterations):
+            new_grid = self.pred_grid.copy()
+            for i in range(0,height):
+                for j in range(0,width):
+                    if(np.isnan(self.original_grid[i,j])):
+                        new_grid[i,j] = self.update_cell(new_grid,i,j)
+        
+            self.pred_grid = new_grid
+        return(self.pred_grid)
+                       
+                    
+    def update_cell(self,grid,i,j):
+        conv_filter = np.array([
+            [0, self.gamma, 0],
+            [self.gamma, 0, self.gamma],
+            [0, self.gamma, 0]
+        ])
+        
+        r1_low = i-1
+        r1_high = i+1
+        c1_low = j-1
+        c1_high = j-1
+        
+        r2_low = 0
+        r2_high = 2
+        c2_low = 0
+        c2_high = 2
+        
+        if(i-1 < 0):
+            r1_low = i
+            r2_low = 1
+        elif(i+1 > grid.shape[0]):
+            r1_high = i
+            r2_high = 1
+        if(j-1 < 0):
+            c1_low = j
+            c2_low = 1
+        elif(j+1 > grid.shape[1]):
+            c1_high = j
+            c2_low = 1
+        
+        vals = np.multiply(grid[r1_low:r1_high,c1_low:c1_high],conv_filter[r2_low:r2_high,c2_low:c2_high])
+        return(np.sum(vals))
+        
+        
+    def find_gamma(self,search_epochs,subsample_proportion,iterations=100,ext=None):
+        """
+        Automatically sets gamma to the best found value. Currently
+        only supports random search.
+        
+        :param search_epochs: number of epochs used by the random search
+        :param subsample_proportion: proportion of training data used to compute errors
+        :param iterations: number of MRP interations used by the random search
+        :returns: best found value for gamma
+        """
+
+        if(ext != None):
+            # Training set
+            pass
+        
+        else:
+            # Subsample
+            
+            sub_grid = self.original_grid.copy()
+            for i in range(0,len(sub_grid)):
+                for j in range(0,len(sub_grid[i])):
+                    if(not(np.isnan(sub_grid[i][j]))):
+                        if(np.random.rand() < subsample_proportion):
+                            sub_grid[i][j] = np.nan
+                                                  
+            best_loss = np.inf
+            best_gamma = 0.9
+            
+            for ep in range(0,search_epochs):
+                # Random search for best gamma for search_epochs iterations
+                
+                temp_MRP = SD_SMRP(sub_grid)
+                gamma = np.random.rand()
+                temp_MRP.set_gamma(gamma)
+                pred_grid = temp_MRP.run(iterations)
+                
+                # Compute MAE of subsampled predictions
+                err = 0
+                err_count = 0
+                for i in range(0,len(self.original_grid)):
+                    for j in range(0,len(self.original_grid[i])):
+                        if(not(np.isnan(self.original_grid[i][j]))):
+                            if(np.isnan(sub_grid[i][j])):
+                                err += abs(pred_grid[i][j] - self.original_grid[i][j])
+                                err_count += 1
+                mae = err / err_count
+                if(mae < best_loss):
+                    best_gamma = gamma
+                    best_loss = mae
+                
+                temp_MRP.reset()
+                
+        self.gamma = best_gamma
+        return(best_gamma)
+        
+    def compute_confidence(self,iterations=100):
+        """
+        Gives a confidence indication (float 0-1) for all cells in the grid by
+        running a sub-MRP interpolation process on a grid where the confidence
+        for known values is set to 1.
+        
+        :param iterations: number of iterations used for running the sub-MRP 
+        :returns: confidence indication per pixel
+        """
+        height = self.original_grid.shape[0]
+        width = self.original_grid.shape[1]
+        new_grid = np.zeros((height,width)) + 1
+        inds = np.isnan(self.original_grid)
+        new_grid[inds] = np.nan
+        
+        temp_MRP = SD_SMRP(new_grid,gamma=self.gamma)
+        confidence_grid = temp_MRP.run(iterations)
+        return(confidence_grid)
+        
+        
+        
+class SD_STMRP(STMRP):
+    """
+    Class for SD-STMRP, extending STMRP
+
+    Attributes
+    ----------
+    original_grid : 2D numpy array
+        the original grid supplied to be interpolated
+    pred_grid : 2D numpy array
+        interpolated version of original_grid
+    G : networkx directed graph
+        graph representation of pred_grid
+    gamma : float
+        spatial discount parameter gamma used by SD-STMRP (typically 0-1
+    tau : float
+        temporal discount parameter tau used by SD-STMRP (typically 0-1)
+
+    Methods
+    -------
+    run():
+        Runs SD-MRP
+        
+    set_gamma():
+        Sets gamma to a user-specified value
+        
+    set_tau():
+        Sets tau to a user-specified value
+        
+    find_gamma():
+        Automatically determines the best gamma (using subsampling or a training set)
+    """
+    
+    def __init__(self,grid,auto_timestamps=False,gamma=0.9,tau=0.9):
+        super(SD_STMRP, self).__init__(grid,auto_timestamps)
+        self.gamma = gamma
+        self.tau = tau
+    
+    def set_gamma(self,gamma):
+        """
+        Sets gamma to the manually supplied value.
+        
+        :param gamma: user-supplied gamma value
+        """
+        self.gamma = gamma
+    
+    def set_tau(self,tau):
+        """
+        Sets tau to the manually supplied value.
+        
+        :param tau: user-supplied tau value
+        """
+        self.tau = tau
+            
+    def run(self,iterations):
+        """
+        Runs SD-STMRP for the specified number of iterations.
+        
+        :param iterations: number of iterations used for the state value update function
+        :returns: interpolated grid pred_grid
+        """
+        for n in self.G.nodes(data=True):
+            r = n[1]['r']
+            c = n[1]['c']
+            y = n[1]['y']
+            E = n[1]['E']
+            
+            if(np.isnan(y)):
+                v_a_sum = 0
+                t = n[1]['t']
+                for n1,n2,w in self.G.in_edges(n[0],data=True):
+                    destination_node = self.G.nodes(data=True)[n1]
+                    E_dest = destination_node['E']
+                    t_dest = destination_node['t']
+                    if(t_dest != t):
+                        discount = self.tau
+                    else:
+                        discount = self.gamma
+                    v_a = discount * E_dest
+                    v_a_sum += v_a
+                E_new = v_a_sum / len(self.G.in_edges(n[0]))
+                nx.set_node_attributes(self.G,{n[0]:E_new},'E')
+
+            else:
+                nx.set_node_attributes(self.G,{n[0]:y},'E')
+        
+        self.update_grid()
+        return(self.pred_grid)
+        
+    def find_discounts(self,search_epochs,subsample_proportion,iterations=100,ext=None):
+        """
+        Automatically sets gamma and tau to the best found value. Currently
+        only supports random search.
+        
+        :param search_epochs: number of epochs used by the random search
+        :param subsample_proportion: proportion of training data used to compute errors
+        :param iterations: number of MRP interations used by the random search
+        :returns: best found value for gamma
+        """
+        
+        if(ext != None):
+            # Training set
+            pass
+        
+        else:
+            # Subsample
+            
+            height = self.original_grid.shape[0]
+            width = self.original_grid.shape[1]
+            depth = self.original_grid.shape[2]
+            
+            sub_grid = self.original_grid.copy()
+            for i in range(0,height):
+                for j in range(0,width):
+                    for t in range(0,depth):
+                        if(not(np.isnan(sub_grid[i][j][t]))):
+                            if(np.random.rand() < subsample_proportion):
+                                sub_grid[i][j][t] = np.nan
+                                       
+            temp_MRP = SD_STMRP(sub_grid)
+            
+            best_loss = np.inf
+            best_gamma = self.gamma
+            best_tau = self.tau
+            
+            for ep in range(0,search_epochs):
+                # Random search for best gamma for search_epochs iterations
+                
+                gamma = np.random.rand()
+                tau = np.random.rand()
+                temp_MRP.set_gamma(gamma)
+                temp_MRP.set_tau(tau)
+                pred_grid = temp_MRP.run(iterations)
+                
+                # Compute MAE of subsampled predictions
+                err = 0
+                err_count = 0
+                               
+                for i in range(0,height):
+                    for j in range(0,width):
+                        for t in range(0,depth):
+                            if(not(np.isnan(self.original_grid[i][j][t]))):
+                                if(np.isnan(sub_grid[i][j][t])):
+                                    err += abs(pred_grid[i][j][t] - self.original_grid[i][j][t])
+                                    err_count += 1
+                mae = err / err_count
+                if(mae < best_loss):
+                    best_gamma = gamma
+                    best_tau = tau
+                    best_loss = mae
+                
+                temp_MRP.reset()
+                
+        self.gamma = best_gamma
+        self.tau = best_tau
+        
+        return(best_gamma, best_tau)
