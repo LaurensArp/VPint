0ed140e012a66a07b728243a069ef04c12a4dbb4
Laurens Arp <l.r.arp@liacs.leidenuniv.nl>
2021-06-11 10:45:56 +0200

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(name='VPint',
-      version='0.1',
+      version='0.2',
       description='Value propagation-based spatial and spatio-temporal interpolation',
       url='https://github.com/LaurensArp/VPint',
       author='Laurens Arp',
       author_email='l.r.arp@liacs.leidenuniv.nl',
       license='GPL-3.0',
-      packages=['VPint', 'utils'],
+      packages=['VPint'],
       install_requires=[
           'numpy',
-          'networkx',
       ],
       zip_safe=False)
